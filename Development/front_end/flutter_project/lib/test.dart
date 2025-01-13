818a940a4aa2217dd2408092c6765a27d7c1ee91
Charlie Fang <charlie.fang0130@gmail.com>
2025-01-13 02:36:38 -0500

    get chem-sample.rpy

--- Development/front_end/flutter_project/lib/test.dart
+++ Development/front_end/flutter_project/lib/test.dart
@@ -2,7 +2,13 @@ import 'package:http/http.dart' as http;
 
 void fetchLogin() async {
   try {
-    final response = await http.get(Uri.parse('https://catopus.education/login/'));
+    var headers = {
+      'Username': 'Fang123',
+      'Token': 'Jek19dnR9KJrn9EAm'
+    };
+
+    final response =
+        await http.get(Uri.parse('https://catopus.education/game-scripts/chem-sample.rpy'), headers: headers);
     if (response.statusCode == 200) {
       print('Response: ${response.body}');
     } else {
