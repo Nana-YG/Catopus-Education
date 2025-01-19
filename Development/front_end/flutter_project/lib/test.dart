b5d9ded669a6e628451e09c8ef1aea09b8b741d8
Charlie Fang <charlie.fang0130@gmail.com>
2025-01-18 22:53:15 -0500

    decode：UTF-8

--- Development/front_end/flutter_project/lib/test.dart
+++ Development/front_end/flutter_project/lib/test.dart
@@ -1,16 +1,22 @@
+import 'dart:convert'; 
 import 'package:http/http.dart' as http;
 
 void fetchLogin() async {
   try {
     var headers = {
       'Username': 'Fang123',
-      'Token': 'Jek19dnR9KJrn9EAm'
+      'Token': 'Jek19dnR9KJrn9EAm',
     };
 
-    final response =
-        await http.get(Uri.parse('https://catopus.education/game-scripts/chem-sample.rpy'), headers: headers);
+    final response = await http.get(
+      Uri.parse('https://catopus.education/game-scripts/chem-sample.rpy'),
+      headers: headers,
+    );
+
     if (response.statusCode == 200) {
-      print('Response: ${response.body}');
+      // 强制解码为 UTF-8
+      final decodedBody = utf8.decode(response.bodyBytes);
+      print('Response: $decodedBody');
     } else {
       print('Failed with status: ${response.statusCode}');
     }
