0e273477e8adb14970b470f9213329df30a6c60f
wxy <wu1765@purdue.edu>
2025-04-11 19:36:47 -0400

    content first try

--- Development/back_end/Login-Container/Login-Server/src/main/java/org/catopus/loginserver/model/CourseRegistration.java
+++ Development/back_end/Login-Container/Login-Server/src/main/java/org/catopus/loginserver/model/CourseRegistration.java
@@ -6,11 +6,11 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.Data;
+import lombok.Data; 
 
 @Data
 @Entity
-@Table(name = "courses")
+@Table(name = "courses") 
 public class CourseRegistration {
     
     @Id
